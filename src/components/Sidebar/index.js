461e26053f3b9d3b867a35d30683ad05b04524c1
Babak Chehraz <bchehraz@csumb.edu>
2018-03-29 14:54:02 -0700

    Refactored Sidebar's div style

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const Sidebar = (props) => (
-<div
-    style={{
-      border: '2px solid #e6e6e6',
-      maxWidth: 960,
-      padding: '0.5rem',
-      marginBottom: '25px'
-    }}
-    >
-    <strong>{props.title}.</strong> {props.description}
-</div>
+  <div
+      style={styles.sideBarDiv}
+  >
+      <strong>{props.title}.</strong> {props.description}
+  </div>
 );
+
+const styles = {
+  sideBarDiv: {
+    border: '2px solid #e6e6e6',
+    maxWidth: 960,
+    padding: '0.5rem',
+    marginBottom: '25px'
+  }
+}
 
 export default Sidebar;
