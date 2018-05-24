1e9559e967c0c6d85ef3163c37563cd81065c096
Babak Chehraz <bchehraz@csumb.edu>
2018-05-24 16:49:44 -0700

    Changed menu style animation and element sizing and other stuff.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import FaHome from 'react-icons/lib/fa/home';
+
 import Link from 'gatsby-link'
 import './index.css';
 
@@ -13,17 +15,31 @@ class Menu extends Component {
 
     return (
       <div
-        id="flyoutMenu"
-        onMouseDown={this.props.handleMouseDown}
+        id="menuContainer"
         className={visibility}
+        onMouseDown={this.props.handleMouseDown}
       >
-        <h2><Link to="/">Home</Link></h2>
-        <h2><Link to="/">About</Link></h2>
-        <h2><Link to="/">Contact</Link></h2>
-        <h2><Link to="/">Search</Link></h2>
+        <div
+          id="flyoutMenu"
+        >
+          <ul>
+            <Link style={styles.menuLink} to="/"><li><h2>Home</h2></li></Link>
+            <Link style={styles.menuLink} to="/"><li><h2>About</h2></li></Link>
+            <Link style={styles.menuLink} to="/"><li><h2>Contact</h2></li></Link>
+            <Link style={styles.menuLink} to="/"><li><h2>Search</h2></li></Link>
+          </ul>
+        </div>
       </div>
     );
   }
+}
+
+const styles = {
+  menuLink: {
+    color: 'black',
+    textDecoration: 'none',
+    margin: 0
+  },
 }
 
 export default Menu;
