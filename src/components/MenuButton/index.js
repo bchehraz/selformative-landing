75c9839a81a1a4dedab00feaa61bec39d9b870b2
Babak Chehraz <bchehraz@csumb.edu>
2018-05-29 14:37:30 -0700

    Removed unused, commented code

--- src/components/MenuButton/index.js
+++ src/components/MenuButton/index.js
@@ -11,12 +11,6 @@ class MenuButton extends Component {
 
   render() {
     return (
-      /*<menu>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </menu>*/
       <div style={this.props.style}>
         <button style={(this.props.menuVisibility) ? { ...styles.buttonStyle, ...styles.buttonStyleOpen } : styles.buttonStyle} onMouseDown={this.props.handleMouseDown}>
           {"MENU"}
