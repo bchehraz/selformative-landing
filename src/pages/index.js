a3df74d67f26d9686a2413384956d796c6c56347
Babak Chehraz <bchehraz@csumb.edu>
2018-05-30 14:05:05 -0700

    Added a different return statement on the first index of blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,33 @@ export default function Index({ data }) {
     <div className="blog-posts">
       {posts
         .filter(post => post.node.frontmatter.title.length > 0)
-        .map(({ node: post }) => {
-          return (
-            <div className="blog-post-preview" key={post.id}>
-              <h1>
-                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-              </h1>
-              <h2>{post.frontmatter.date}</h2>
-              <p>{post.excerpt}</p>
-            </div>
-          );
+        .map(({ node: post }, index) => {
+          console.log(index);
+
+          //The most recent blog post is shown larger than the rest
+          if (index === 0) {
+            return (
+              <div className="blog-post-preview" key={post.id}>
+                <h1>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                </h1>
+                <h2>{post.frontmatter.date}</h2>
+                <iframe src={post.frontmatter.src}
+            width="560" height="315" frameborder="0"></iframe>
+              </div>
+            );
+          } else {
+            return (
+              <div className="blog-post-preview" key={post.id}>
+                <h1>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                </h1>
+                <h2>{post.frontmatter.date}</h2>
+                <iframe src={post.frontmatter.src}
+            width="560" height="315" frameborder="0"></iframe>
+              </div>
+            );
+          }
         })}
     </div>
   );
@@ -30,12 +47,12 @@ export const pageQuery = graphql`
     allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
       edges {
         node {
-          excerpt(pruneLength: 250)
           id
           frontmatter {
             title
             date(formatString: "MMMM DD, YYYY")
             path
+            src
           }
         }
       }
